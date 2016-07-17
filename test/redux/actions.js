b43722d0ae9743b069227340470dffdec2b2c92a
Nathan Gaberel <nathang@theodo.co.uk>
2016-07-17 16:06:58 +0100

    Test actions creators.

---
+++ test/redux/actions.js
@@ -0,0 +1,55 @@
+import {expect} from 'chai';
+
+import {
+  types,
+  addFile,
+  removeFile,
+  toggleAutoCompile
+} from '../../src/redux/actions';
+import {Jade} from '../../src/services/languages';
+
+describe('Action creators', () => {
+  describe('addFile()', () => {
+    let action;
+
+    beforeEach(() => {
+      action = addFile('/yoyoyo.pug');
+    });
+
+    it('generates an action with the correct type', () => {
+      expect(action).to.have.property('type', types.ADD_FILE);
+    });
+
+    it('generates an action with source and destination info', () => {
+      expect(action).to.have.deep.property('file.source', '/yoyoyo.pug');
+      expect(action).to.have.deep.property('file.dest', '/yoyoyo.html');
+    });
+
+    it('detects the file type', () => {
+      expect(action).to.have.deep.property('file.type', Jade.name);
+    });
+  });
+
+  describe('removeFile()', () => {
+    let action;
+
+    beforeEach(() => {
+      action = removeFile('/yoyoyo.pug');
+    });
+
+    it('generates an action with the correct type', () => {
+      expect(action).to.have.property('type', types.REMOVE_FILE);
+    });
+
+    it('generates an action with the file path', () => {
+      expect(action).to.have.property('file', '/yoyoyo.pug');
+    });
+  });
+
+  describe('toggleAutoCompile()', () => {
+    it('generates an action with the correct type', () => {
+      const action = toggleAutoCompile();
+      expect(action).to.have.property('type', types.TOGGLE_AUTO_COMPILE);
+    });
+  });
+});
