428138b1e43a60cb3c8e9569f654aeee6e1f5cc7
Nathan Gaberel <nathan@gnab.fr>
2018-08-11 16:24:55 +0100

    Remove old tests.

--- test/helper.js
+++
@@ -1,8 +0,0 @@
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import chaiImmutable from 'chai-immutable';
-
-import 'ignore-styles'
-
-chai.use(chaiEnzyme());
-chai.use(chaiImmutable);
